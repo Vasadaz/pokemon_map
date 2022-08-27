599a9ca7d4c3ea5c2ab11f8f58784bf246e14f86
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-27 15:36:54 +0300

    Add Pokemon.image model field.

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Pokemon
 
-admin.site.register(Pokemon)
+
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image')
+
+admin.site.register(Pokemon, PokemonAdmin)
