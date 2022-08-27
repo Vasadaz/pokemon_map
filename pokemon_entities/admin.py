0264c649a29b38648876fbbadb836402dd11461c
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-08-27 16:00:30 +0300

    Add PokemonEntity model.

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ('title', 'image')
 
+
+class PokemonEntityAdmin(admin.ModelAdmin):
+    list_display = ('pokemon', 'lat', 'long')
+
+
 admin.site.register(Pokemon, PokemonAdmin)
+admin.site.register(PokemonEntity, PokemonEntityAdmin)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,3 +7,16 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name='Pokemon',
+        on_delete=models.CASCADE,
+    )
+    lat = models.FloatField(verbose_name='Latitude')
+    long = models.FloatField(verbose_name='Longitude')
+
+    def __str__(self):
+        return f'{self.pokemon} on the map by ({self.lat}, {self.long})'
